fdd4c17bd6c5384aae0e9aecb25385f6da19d506
Conrad Khakhria <conradjkhakhria@gmail.com>
2024-02-09 12:48:25 +0000

    eh

--- Coursework_01/Code/grid_search/dijkstra_planner.py
+++ Coursework_01/Code/grid_search/dijkstra_planner.py
@@ -5,13 +5,14 @@ Created on 2 Jan 2022
 '''
 
 from collections import deque
-from math import sqrt
+from math import sqrt, isinf
 from queue import PriorityQueue
 from typing import Optional, Tuple
 
 from .occupancy_grid import OccupancyGrid
 from .planner_base import PlannerBase
-from .search_grid import SearchGridCell
+from .search_grid import SearchGrid, SearchGridCell
+from .search_grid_drawer import SearchGridDrawer
 
 class DijkstraPlanner(PlannerBase):
 
@@ -21,47 +22,38 @@ class DijkstraPlanner(PlannerBase):
     def __init__(self, occupancy_grid: OccupancyGrid):
         PlannerBase.__init__(self, occupancy_grid)
         self.priority_queue = PriorityQueue()  # type: ignore
-        self.cumulative_distances = []
-
         self.searchCount = 0
         self.maxNodesStored = 0
-    
-        for _ in range(occupancy_grid.height()):
-            self.cumulative_distances.append([float("inf")]*occupancy_grid.width())
+        self.start_added_to_queue = False
 
 
     def push_cell_onto_queue(self, cell: SearchGridCell):
-        x, y = cell.coords()
+        if cell.parent:
+            cell.path_cost = cell.parent.path_cost + self.compute_l_stage_additive_cost(cell.parent, cell)
+        else:
+            cell.path_cost = 0
 
-        self.searchCount += 1
-
-        if cell.is_start:
-            self.cumulative_distances[y][x] = 0
-
-        node_len = self.priority_queue.qsize()
-        if node_len > self.maxNodesStored:
+        if (node_len := self.priority_queue.qsize()) > self.maxNodesStored:
             self.maxNodesStored = node_len
 
-        self.priority_queue.put((self.cumulative_distances[y][x], cell))
+        self.priority_queue.put((cell.path_cost, cell))
+        self.searchCount += 1
 
 
     def is_queue_empty(self) -> bool:
-        return self.priority_queue.empty()
-    
+        return self.priority_queue.qsize() == 0
+
 
     def pop_cell_from_queue(self) -> SearchGridCell:
         return self.priority_queue.get()[1]
 
 
-    def has_goal_been_reached(self, cell) -> bool:
-        return False
-
-
     def resolve_duplicate(self, cell: SearchGridCell, parent_cell: SearchGridCell):
-        px, py = parent_cell.coords()
-        x, y = cell.coords()
+        tentative = parent_cell.path_cost + self.compute_l_stage_additive_cost(parent_cell, cell)
 
-        tentative = self.cumulative_distances[py][px] + sqrt((x - px)**2 + (y - py)**2)
-
-        if tentative < self.cumulative_distances[y][x]:
-            self.cumulative_distances[y][x] = tentative
+        if tentative < cell.path_cost:
+            cell.parent = parent_cell
+            if (cell.path_cost, cell) in self.priority_queue.queue:
+                self.priority_queue.queue.remove((cell.path_cost, cell))
+                cell.path_cost = tentative
+                self.priority_queue.put((cell.path_cost, cell))
